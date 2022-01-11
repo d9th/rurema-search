013e9aa6a1f35dc487527c2a57abab84e4ce0000
Shunsuke Shiina <gravitate.karma@gmail.com>
2022-01-11 18:01:08 +0900

    3.1.0用に修正

--- rurema-search.js
+++ rurema-search.js
@@ -6,15 +6,18 @@
 // /query:foo/query:bar/へ変換してるくれてるようだ
 // ?query=foo+barでリクエストしても大丈夫
 
-const RUBY_VERSION = "3.0.0";
+const RUBY_VERSION = "3.1.0";
 
-const searchKeyword = Deno.args.join(" ");
+const argsOfKeyword = Deno.args;
+const queryByKeyword = argsOfKeyword.reduce((prev, curr) => {
+  return (prev += `query:${curr}/`);
+}, "");
 
 const url = new URL(
-  `https://docs.ruby-lang.org/ja/search/api:v1/version:${RUBY_VERSION}/`
+  `https://docs.ruby-lang.org/ja/search/api:v1/version:${RUBY_VERSION}/${queryByKeyword}`
 );
 
-url.searchParams.set("query", searchKeyword);
+// url.searchParams.set("query", searchKeyword);
 
 const response = await fetch(url, {
   redirect: "follow",
@@ -26,10 +29,13 @@ const response = await fetch(url, {
 if (response.ok) {
   const { entries } = await response.json();
   const resultOfSearch = entries.slice(0, 9).map(entry => {
-    const url = entry.documents[0].url.replace(
+    let url = entry.documents[0].url.replace(
       "docs.ruby-lang.org/ja/search/http://",
       ""
     );
+    // 3.1.0では404を返すので3.1に置換処理をする
+    url = url.replace("3.1.0", "3.1");
+
     return {
       title: entry.signature,
       subtitle: entry.summary ?? "No summary",
