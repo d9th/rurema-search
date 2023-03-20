03889a91995c199325e0205ce7325e3b7664e913
d9th <gravitate.karma+github@gmail.com>
2023-03-20 10:33:03 +0900

    changed base url.

--- rurema-search.js
+++ rurema-search.js
@@ -1,25 +1,47 @@
 // rurema search plugin for Alfred
 // rurema API
-// https://docs.ruby-lang.org/ja/search/api:v1/version:3.0.0/query:string/query:length/
+// https://docs.ruby-lang.org/ja/search/api:v1/version:3.1.0/query:string/query:length/
 
 // ruremaはquery=foo+barでリクエストすると
 // /query:foo/query:bar/へ変換してるくれてるようだ
 // ?query=foo+barでリクエストしても大丈夫
 
-const RUBY_VERSION = "3.1.0";
+const RUBY_VERSION = "3.2.0";
 
-const argsOfKeyword = Deno.args;
-const queryByKeyword = argsOfKeyword.reduce((prev, curr) => {
-  return (prev += `query:${curr}/`);
-}, "");
+function generateQueryByKeyword(keywords) {
+  return keywords.reduce((prev, curr) => {
+    return (prev += `query:${curr}/`);
+  }, "");
+}
 
-const url = new URL(
-  `https://docs.ruby-lang.org/ja/search/api:v1/version:${RUBY_VERSION}/${queryByKeyword}`
-);
+function generateSearchUrl(keywords, version = RUBY_VERSION) {
+  const query = generateQueryByKeyword(keywords);
+  const baseUrl = "https://rurema.clear-code.com/api:v1";
+  // `https://docs.ruby-lang.org/ja/search/api:v1/version:${version}/${query}`
+  const url = new URL(`${baseUrl}/version:${version}/${query}`);
+  return url;
+}
+
+function replaceUrl(url) {
+  return url.replace("docs.ruby-lang.org/ja/search/http://", "");
+  // .replace(RUBY_VERSION, "3.2");
+}
+
+function createSearchResult(entry) {
+  const url = replaceUrl(entry.documents[0].url);
+  return {
+    title: entry.signature,
+    subtitle: entry.summary ?? "No summary",
+    arg: url,
+    url,
+  };
+}
+
+const searchKeywords = Deno.args;
+const url = generateSearchUrl(searchKeywords);
 
 try {
   const response = await fetch(url, {
-    redirect: "follow",
     headers: {
       "Content-Type": "application/json",
     },
@@ -30,27 +52,8 @@ try {
   }
 
   const { entries } = await response.json();
-  const resultOfSearch = entries.slice(0, 9).map(entry => {
-    let url = entry.documents[0].url.replace(
-      "docs.ruby-lang.org/ja/search/http://",
-      ""
-    );
-    // 3.1.0では404を返すので3.1に置換処理をする
-    url = url.replace("3.1.0", "3.1");
-
-    return {
-      title: entry.signature,
-      subtitle: entry.summary ?? "No summary",
-      arg: url,
-      url,
-    };
-  });
-
-  const alfredJson = {
-    items: resultOfSearch,
-  };
-
-  console.log(JSON.stringify(alfredJson));
+  const result = entries.slice(0, 9).map(entry => createSearchResult(entry));
+  console.log(JSON.stringify({ item: result }));
 } catch (error) {
   console.error(error);
 }
