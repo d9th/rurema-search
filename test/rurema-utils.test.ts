0ef9b856bb8639eb669621541d00971c9e96344d
d9th <gravitate.karma+github@gmail.com>
2024-01-01 14:40:13 +0900

    Update Deno formatter configuration and add tests for search result generation

--- test/rurema-utils.test.ts
+++ test/rurema-utils.test.ts
@@ -2,9 +2,37 @@ import {
   assertEquals,
   assertInstanceOf,
 } from "https://deno.land/std@0.210.0/assert/mod.ts";
+import { createSearchResult, generateSearchUrl } from "../utils/rurema-utils.ts";
 
-import { generateSearchUrl } from "../utils/rurema-utils.ts";
-
+const entry = {
+  signature: "Array#each -> Enumerator",
+  score: 81415.0,
+  metadata: { type: "instance-method", versions: ["3.2.0"] },
+  summary: "各要素に対してブロックを評価します。",
+  documents: [
+    {
+      version: "3.2.0",
+      url: "https://rurema.clear-code.com/3.2.0/method/Array/i/each.html",
+      description:
+        "各要素に対してブロックを評価します。\n\nブロックが与えられなかった場合は、自身と each から生成した\nEnumerator オブジェクトを返します。\n\n//emlist[例][ruby]{\n[1, 2, 3].each do |i|\n  puts i\nend\n#=> 1\n#   2\n#   3\n//}\n\n\n@see Array#each_index, Array#reverse_each",
+      snippets: [],
+    },
+  ],
+  related_entries: [
+    {
+      key: "Array",
+      label: "Array",
+      type: "class",
+      url: "https://rurema.clear-code.com/api:v1/class:Array/",
+    },
+    {
+      key: "Enumerator",
+      label: "Enumerator",
+      type: "query",
+      url: "https://rurema.clear-code.com/api:v1/query:Enumerator/",
+    },
+  ],
+};
 Deno.test("generateSearchUrlはURLオブジェクトを返す", () => {
   const keywords = ["String", "length"];
   assertInstanceOf(generateSearchUrl(keywords), URL);
@@ -28,3 +56,13 @@ Deno.test(
     assertEquals(generateSearchUrl(keywords).toString(), expectedUrl);
   },
 );
+Deno.test("createSearchResult should return expected search result.", () => {
+  const result = createSearchResult(entry);
+  const expected = {
+    title: "Array#each -> Enumerator",
+    subtitle: "各要素に対してブロックを評価します。",
+    arg: "https://rurema.clear-code.com/3.2.0/method/Array/i/each.html",
+    url: "https://rurema.clear-code.com/3.2.0/method/Array/i/each.html",
+  };
+  assertEquals(result, expected);
+});
